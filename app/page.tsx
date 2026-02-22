f2f55af6654db5f8d2cb042d71b86a99fcb30233
Daniele Chiaf <28843028+Chiaf1@users.noreply.github.com>
2026-02-22 20:06:20 +0100

    playing with colors

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black ">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div className="flex min-h-screen items-center justify-center bg-[#E2E4DD] font-sans text-[#242628]">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center py-32 px-16 bg-[#E2E4DD] sm:items-start">
         <div className="flex w-full flex-col items-center ">
-            <h1 className="flex text-3xl font-bold text-[#F9CE1F] text-center ">
+            <h1 className="flex text-3xl font-bold text-[#FFB200] text-center ">
               Hi this is the start of my personal website
             </h1>
         </div>
